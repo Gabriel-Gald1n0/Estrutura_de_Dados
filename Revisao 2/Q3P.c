e45207f53865a309169df3c66ffcab4af7912ec9
gabriel <galdinobrossx1@gmail.com>
2024-12-02 15:35:02 -0300

    correcao prova 2

--- Revisao 2/Q3P.c
+++ Revisao 2/Q3P.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void moda(int v[], int n);
+void prog(int v[], int n);
+void bobbleSort(int v[], int n);
 
 int main(){
     int n = 7;
-    int v[] = { 3,3,3,4,4,5,7};
+    int v[] = { 4,5,3,4,3,3,7};
     
-    moda(v,n);
+    bobbleSort(v,n);
+    prog(v,n);
     return 0;
 }
 
-void moda(int v[], int n){
+void prog(int v[], int n){
     int *newV;
     newV = (int*) calloc(n,sizeof(int));
     int res = n/3;
@@ -32,13 +34,22 @@ void moda(int v[], int n){
 
     for(int i = 0; i < n; i++){
         if(newV[i] >= res){
-            if(i == 0 || v[i] != v[i-1]){
+            if(i == 0 || v[i] != v[i-1]){ //// errei apenas o sinal dentro do v[i - 1] e acabei me confundindo e colocando v[i+1]
                 printf("%d ", v[i]);
             }
         }
     }
-
-   
-    
     free(newV);
+}
+
+void bobbleSort(int v[], int n){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(v[j] > v[j+1]){
+                int temp = v[j];
+                v[j] = v[j+1];
+                v[j+1] = temp;
+            }
+        }
+    }
 }
